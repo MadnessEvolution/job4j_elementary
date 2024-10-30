c5607fb829b97540f572f86e5f0120002d22cca3
Oleg <lyffi.run@gmail.com>
2024-10-31 00:57:36 +0300

    Разбор первой программы по "взрослому" [#505151]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public class void main(String[] args) {
+        System.out.println("Oleg Dorogov");
+        System.out.println("19.05.1996");
+    }
+}
