b233389b199b7cbf4be4e287ac2dccf946ecb2f6
Oleg <lyffi.run@gmail.com>
2024-11-16 21:42:24 +0300

    Тестирование Класса Калькулятора Температуры Хранения [#505279]

--- src/main/java/ru/job4j/calculator/TemperatureFit.java
+++ src/main/java/ru/job4j/calculator/TemperatureFit.java
@@ -3,17 +3,15 @@ package ru.job4j.calculator;
 public class TemperatureFit {
 
     public static double idealTemperatureForFruits(int temperature) {
-        // Ideal temperature for storing fruits in Celsius
-        return temperature - 2.0;
+        return temperature - 12.0;
     }
 
     public static double idealTemperatureForMeat(int temperature) {
-        // Ideal temperature for storing meat in Celsius
-        return temperature - 5.0;
+        return temperature - 15.0;
     }
 
     public static void main(String[] args) {
-        int temperature = 10; // Example room temperature
+        int temperature = 20; // Example room temperature
         double fruitsTemp = TemperatureFit.idealTemperatureForFruits(temperature);
         double meatTemp = TemperatureFit.idealTemperatureForMeat(temperature);
         System.out.println("Ideal temperature for fruits is " + fruitsTemp + "°C");
