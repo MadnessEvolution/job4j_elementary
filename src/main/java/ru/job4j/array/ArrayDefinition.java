fad8b16fa1d5f5b2bae872d4e7d8a3fd2ce342fe
Oleg <lyffi.run@gmail.com>
2025-02-25 22:02:30 +0300

    Тест

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -10,7 +10,7 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + prices.length);
         String[] names = new String[5];
         names[0] = "Katya";
-        names[1] = "Sasha1";
+        names[1] = "Sasha";
         names[2] = "Kostya";
         names[3] = "Akim";
         names[4] = "Dasha";
